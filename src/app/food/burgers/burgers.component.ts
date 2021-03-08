bbbd1b8f8dbcd89283da146e75bbf74a4fb17bb6
nparsley <nickparsley5@gmail.com>
2021-03-07 22:38:02 -0700

    menuitem-property binding setup + button add

--- src/app/food/burgers/burgers.component.ts
+++ src/app/food/burgers/burgers.component.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./burgers.component.css']
 })
 export class BurgersComponent implements OnInit {
+  burgerType = [
+    {
+      name: 'BBQ Bacon',
+      description: 'bacon, american cheese, onions, pickles, and BBQ sauce',
+      price: 13.00
+    },
+    {
+      name: 'Swiss Avocado',
+      description: 'swiss cheese, avocado slices, and tomatoes',
+      price: 11.00
+    },
+    {
+      name: 'Blue Cheese',
+      description: 'blue cheese, fried onion strings, jalapenos and spicy ketchup',
+      price: 12.00
+    },
+    {
+      name: 'Cheese Burger',
+      description: 'american cheese, lettuce, tomatoes, onions, and pickles',
+      price: 10.00
+    }
+  ];
+  cartBurger = [];
 
   constructor() { }
 
--- src/app/food/pizza/pizza.component.ts
+++ src/app/food/pizza/pizza.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pizza',
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizza.component.css']
 })
 export class PizzaComponent implements OnInit {
+  pizzaType = [
+    {
+      name: 'Margherita',
+      description: 'tomatoes, mozzarella, and fresh basil',
+      price: 15.00
+    },
+    {
+      name: 'Pepperoni',
+      description: 'pepperoni, mozzarella, and olives',
+      price: 17.00
+    },
+    {
+      name: 'Cheese',
+      description: 'mozzarella, gorgonzola, fontina, and parmigiano',
+      price: 12.00
+    },
+    {
+      name: 'Veggie',
+      description: 'mushrooms, green peppers, tomatoes, black olives, and onions',
+      price: 14.00
+    }
+  ];
+  cartPizza = [];
 
   constructor() { }
 
